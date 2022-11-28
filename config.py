b661634f566b057a6df4d593bfc6885b0567eae4
ananmkabaha <anan.kabaha@gmail.com>
2022-11-28 10:50:05 +0000

    config file

---
+++ config.py
@@ -0,0 +1,13 @@
+class config:
+    netname = None 
+    epsilon = 0 
+    dataset = None
+    max_itr = 1000
+    M = 3
+    pertubration_type = "brightness"
+    default_steps = [0.5e-3, 1e-3]
+    TimeOut = 15000
+    gpus = "0,1,2,3,4,5,6,7"
+    save_figures = False
+    output_file = "./results.txt"
+    
